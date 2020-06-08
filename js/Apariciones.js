1b6cd620beef9b6c885842c93296ad5a470eb8dc
AntonioPadilla <antoniopadilla.path@gmail.com>
2020-06-07 20:57:07 -0500

    Commit Inicial

---
+++ js/Apariciones.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+	$("#Welcome").hide().show(1500);
+});
+$(document).ready(function() {
+	$("#Barra").hide().delay(2000).show(1000);
+});
+$(document).ready(function() {
+	$(".Superheroes").hide().delay(3000).show(1000);
+});
+$(document).ready(function() {
+	$(".Princess").hide().delay(3000).show(1000);
+});
+$(document).ready(function() {
+	$("footer").hide().delay(3000).show(1000);
+});
+$(document).ready(function() {
+	$(".Buscar").hide().delay(3000).show(1000);
+});
+$(document).ready(function() {
+	$(".Phone").hide().delay(3000).show(1000);
+});
---
+++ js/AparicionesInicio.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+	$("#Barra").hide().show(1000);
+});
+$(document).ready(function() {
+	$(".Superheroes").hide().delay(1000).show(1000);
+});
+$(document).ready(function() {
+	$(".Princess").hide().delay(1000).show(1000);
+});
+$(document).ready(function() {
+	$("footer").hide().delay(1000).show(1000);
+});
+$(document).ready(function() {
+	$(".Buscar").hide().delay(1000).show(1000);
+});
+$(document).ready(function() {
+	$(".Phone").hide().delay(1000).show(1000);
+});
